86387d5ed56eec528dd2aeb044456d55c5a57218
belese <johan@solidbot.com>
2020-05-24 14:53:35 +0200

    update homepage

---
+++ precache-manifest.b171dafc750598b17b1874b46a5abc23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b43fa48fbad79a2b9c1cc007da9aeb3",
+    "url": "./react-polapi/index.html"
+  },
+  {
+    "revision": "eed457753b18e4c01a7a",
+    "url": "./react-polapi/static/css/2.97a9311d.chunk.css"
+  },
+  {
+    "revision": "bb4d64ccd186e301e93d",
+    "url": "./react-polapi/static/css/main.e23523fe.chunk.css"
+  },
+  {
+    "revision": "eed457753b18e4c01a7a",
+    "url": "./react-polapi/static/js/2.9fd35825.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "./react-polapi/static/js/2.9fd35825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4d64ccd186e301e93d",
+    "url": "./react-polapi/static/js/main.32760bdd.chunk.js"
+  },
+  {
+    "revision": "26bd201c196508c3afad",
+    "url": "./react-polapi/static/js/runtime-main.40194dca.js"
+  }
+]);
