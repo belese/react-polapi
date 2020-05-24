50259f65e22515d271960ed71cc7080ff81bbc57
belese <johan@solidbot.com>
2020-05-24 14:44:43 +0200

    first commit

---
+++ precache-manifest.a775dff355d3743645e82fb7b47ba81b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbb309c6637a9819afecdfe9c3e8dbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995705fe4b9c5218c107",
+    "url": "/static/css/2.97a9311d.chunk.css"
+  },
+  {
+    "revision": "bb4d64ccd186e301e93d",
+    "url": "/static/css/main.e23523fe.chunk.css"
+  },
+  {
+    "revision": "995705fe4b9c5218c107",
+    "url": "/static/js/2.d8604aaa.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/static/js/2.d8604aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4d64ccd186e301e93d",
+    "url": "/static/js/main.32760bdd.chunk.js"
+  },
+  {
+    "revision": "bb85af023cb1d9c13d4e",
+    "url": "/static/js/runtime-main.60d69362.js"
+  }
+]);
